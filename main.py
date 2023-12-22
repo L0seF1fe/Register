7e5260f0605380082341492041694f6d3c1b4b6e
L0seF1fe <kats10102008@gmail.com>
2023-12-22 19:14:07 +0300

    Code Main.py
    Fixed the naming of fields in the code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
-Login = str(input("Enter your nickname: "))
+Login = str(input("Enter your Nickname: "))
 Password = int(input("Enter your Password: "))
-File_location = r"C:\Users\User\Desktop\Register\Register-and-Login\NamePassword"
+File_location = r"C:\Users\User\Desktop\Register\Register-and-Login\NamePassword.txt"
 class Pattern_Register:
     Login: list
     Password: list
@@ -15,13 +15,13 @@ class Pattern_Register:
     def entering_data(self, Login: str, Password: int, File_location: str):
         try:
             with open(self.File_location, 'a') as data_file:
-                data_file.write(f"{self.Login}':'{self.Password}\n")
+                data_file.write(f"{self.Login}:{self.Password}\n")
                 data_file.close()
         except Exception:
             print("Аккаунт не Зарегистрироване по неизвестной ошибке")
             time.sleep(5)
             print("Аккаунт успешно создан")
             time.sleep(5)
-    entering_data()
 
 Register = Pattern_Register(Login, Password, File_location)
+Register.entering_data(Login, Password, File_location)
