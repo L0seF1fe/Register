5a802d57f6174eee18f1848bae2d1c698ea9cc77
L0seF1fe <kats10102008@gmail.com>
2023-12-22 19:12:04 +0300

    Code Main.py
    I wrote the main code for registration

---
+++ main.py
@@ -0,0 +1,27 @@
+import time
+Login = str(input("Enter your nickname: "))
+Password = int(input("Enter your Password: "))
+File_location = r"C:\Users\User\Desktop\Register\Register-and-Login\NamePassword"
+class Pattern_Register:
+    Login: list
+    Password: list
+    File_location = str
+
+    def __init__(self, Login: str, Password: int, File_location: str):
+        self.Login = Login
+        self.Password = Password
+        self.File_location = File_location
+
+    def entering_data(self, Login: str, Password: int, File_location: str):
+        try:
+            with open(self.File_location, 'a') as data_file:
+                data_file.write(f"{self.Login}':'{self.Password}\n")
+                data_file.close()
+        except Exception:
+            print("Аккаунт не Зарегистрироване по неизвестной ошибке")
+            time.sleep(5)
+            print("Аккаунт успешно создан")
+            time.sleep(5)
+    entering_data()
+
+Register = Pattern_Register(Login, Password, File_location)
